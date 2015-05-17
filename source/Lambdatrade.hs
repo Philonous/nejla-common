afd55d03f8312b2f801f8d12f53c17f78a9744c9
Jon Kristensen <jon.kristensen@lambdatrade.com>
2015-05-17 18:54:32 +0200

    Cabalize; add Persistent instances of UUID and the `withPool' function

---
+++ source/Lambdatrade.hs
@@ -0,0 +1,48 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+-- | In addition to the entities below, this module provides a @PersistField@
+-- and a @PersistFieldSql@ instance of @UUID@.
+module Lambdatrade (withPool) where
+
+import Control.Applicative
+import Control.Monad.IO.Class
+import Control.Monad.Logger
+import Data.Monoid
+import Data.UUID
+import Database.Persist.Postgresql
+import Database.Persist.Sql
+import Database.Persist.TH
+import System.Environment
+
+import Data.ByteString (ByteString)
+
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Lazy as BSL
+import qualified Data.Text as TS
+import qualified Data.Text.Encoding as TS
+
+instance PersistField UUID where
+    toPersistValue = toPersistValue . BS.concat . BSL.toChunks . toByteString
+    fromPersistValue = \x -> fromPersistValue x >>= \v ->
+        case fromByteString $ BSL.fromChunks [v] of
+            Nothing -> Left $ TS.concat ["Invalid UUID: ", TS.pack (show v)]
+            Just u -> Right u
+
+instance PersistFieldSql UUID where
+    sqlType _ = SqlBlob
+
+-- | Acquires the database password (from the @DB_PASSWORD@ environment
+-- variable) and creates a PostgreSQL connection pool with the specified number
+-- of threads.
+withPool :: Int -> (ConnectionPool -> LoggingT IO b) -> IO b
+withPool n f = do
+    dbPassword <- TS.encodeUtf8 . TS.pack <$> getEnv "DB_PASSWORD"
+    (runStderrLoggingT . withPostgresqlPool (connectionString dbPassword) n) f
+  where
+    connectionString passwd = BS.intercalate " "
+                              $ [ "host"     .= "database"
+                                , "user"     .= "lambdatrade"
+                                , "dbname"   .= "lambdatrade"
+                                , "password" .= passwd
+                                ]
+    k .= v = k <> "=" <> v
