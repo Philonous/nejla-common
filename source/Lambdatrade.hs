d3f77b98a0dcdc9eb4fcb262570ecea9c8fca0dc
Jon Kristensen <jon.kristensen@lambdatrade.com>
2015-05-25 21:32:03 +0200

    Add FromJSON, ToJSON and PathPiece instances of UUID

--- source/Lambdatrade.hs
+++ source/Lambdatrade.hs
@@ -1,18 +1,22 @@
 {-# LANGUAGE OverloadedStrings #-}
 
--- | In addition to the entities below, this module provides a @PersistField@
--- and a @PersistFieldSql@ instance of @UUID@.
+-- | In addition to the entities below, this module provides a 'PersistField'
+-- and a 'PersistFieldSql' instance of 'UUID'. It also provides 'FromJSON',
+-- 'ToJSON' and 'PathPiece' instances of UUID.
 module Lambdatrade (withPool) where
 
 import Control.Applicative
+import Control.Monad
 import Control.Monad.IO.Class
 import Control.Monad.Logger
+import Data.Aeson
 import Data.Monoid
 import Data.UUID
 import Database.Persist.Postgresql
 import Database.Persist.Sql
 import Database.Persist.TH
 import System.Environment
+import Web.PathPieces
 
 import Data.ByteString (ByteString)
 
@@ -30,6 +34,16 @@ instance PersistField UUID where
 
 instance PersistFieldSql UUID where
     sqlType _ = SqlBlob
+
+instance ToJSON UUID where
+    toJSON = toJSON . toString
+
+instance FromJSON UUID where
+    parseJSON = maybe mzero return . fromString <=< parseJSON
+
+instance PathPiece UUID where
+    fromPathPiece = fromString . TS.unpack
+    toPathPiece = TS.pack . toString
 
 -- | Acquires the database password (from the @DB_PASSWORD@ environment
 -- variable) and creates a PostgreSQL connection pool with the specified number
