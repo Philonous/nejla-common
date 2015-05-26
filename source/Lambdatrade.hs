247a09a3f8f0f17a2f460f6a092cdcf4b4a998d6
Jon Kristensen <jon.kristensen@lambdatrade.com>
2015-05-26 22:20:33 +0200

    Add mkGenericJSON and JSONSchema instance

--- source/Lambdatrade.hs
+++ source/Lambdatrade.hs
@@ -1,20 +1,28 @@
+-- Copyright © 2014-2015 Lambdatrade AB. All rights reserved.
+
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
 
 -- | In addition to the entities below, this module provides a 'PersistField'
 -- and a 'PersistFieldSql' instance of 'UUID'. It also provides 'FromJSON',
--- 'ToJSON' and 'PathPiece' instances of UUID.
-module Lambdatrade (withPool) where
+-- 'ToJSON', 'JSONSchema' and 'PathPiece' instances of 'UUID'.
+module Lambdatrade (mkGenericJSON, withPool) where
 
 import Control.Applicative
 import Control.Monad
 import Control.Monad.IO.Class
 import Control.Monad.Logger
 import Data.Aeson
+import Data.Char
+import Data.JSON.Schema
 import Data.Monoid
 import Data.UUID
 import Database.Persist.Postgresql
 import Database.Persist.Sql
 import Database.Persist.TH
+import Generics.Generic.Aeson
+import Language.Haskell.TH
+import Language.Haskell.TH.Syntax
 import System.Environment
 import Web.PathPieces
 
@@ -41,6 +49,9 @@ instance ToJSON UUID where
 instance FromJSON UUID where
     parseJSON = maybe mzero return . fromString <=< parseJSON
 
+instance JSONSchema UUID where
+    schema uuid = schema $ fmap (TS.pack . show) uuid
+
 instance PathPiece UUID where
     fromPathPiece = fromString . TS.unpack
     toPathPiece = TS.pack . toString
@@ -60,3 +71,29 @@ withPool n f = do
                                 , "password" .= passwd
                                 ]
     k .= v = k <> "=" <> v
+
+-- | Create "trivial" instances for FromJSON, ToJSON, JSONSchema. The Instance
+-- members are set to gparseJsonWithSettings, gtoJsonWithSettings and
+-- gSchemaWithSettings respectively with options set to strip the type name as a
+-- prefix.
+mkGenericJSON tp = do -- TODO: Rename to "mkGenericJson", probably.
+    t <- tp
+    -- If the type is a simple type, strip its name from the field names
+    let prefix =
+            case t of
+             ConT tpnm -> Just . downcase $ nameBase tpnm
+             _ -> Nothing
+        settings = [| Settings{stripPrefix = $(lift prefix)} |]
+    fj <- [d| instance FromJSON $tp where
+                 parseJSON = gparseJsonWithSettings $settings |]
+    tj <- [d| instance ToJSON $tp where
+                 toJSON = gtoJsonWithSettings $settings |]
+    sc <- [d| instance JSONSchema  $tp where
+                 schema = gSchemaWithSettings $settings |]
+    return . concat $ [ fj
+                      , tj
+                      , sc
+                      ]
+  where
+    downcase [] = []
+    downcase (c:cs) = toLower c : cs
