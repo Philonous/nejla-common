9af8963c96e4484e3d6134fb0c8965a50b6c2f63
Philipp Balzarek <p.balzarek@googlemail.com>
2016-01-26 16:22:50 +0100

    pass filenames as request body in multipart override
    Add some documentation
    
    (#6)

--- source/Lambdatrade/WAI.hs
+++ source/Lambdatrade/WAI.hs
@@ -8,27 +8,36 @@ import           Data.ByteString (ByteString)
 import qualified Data.ByteString as BS
 import           Data.Text (Text)
 import qualified Data.Text as Text
+import qualified Data.Text.Encoding as Text
 import           Network.HTTP.Types
 import           Network.Wai
 import           Network.Wai.Parse
 
-multipartHandlerOverride :: (MonadIO m, MonadBaseControl IO m) =>
-                            Method
+
+-- | Given a method and a path, this middleware will write each part of the
+-- (multipart) request body of requests to the according method and request path
+-- to a file and pass on the filename of the _first_ part in the request body
+--
+-- Example usage with rest-core:
+--
+-- >>>  Warp.run port $ multipartHandlerOverride "POST"
+-- >>>                $ apiToApplication run api
+--
+multipartHandlerOverride :: Method
                          -> [Text]
-                         -> ((Response -> ResponseReceived)
-                             -> [Param]
-                             -> [File FilePath]
-                             -> ResourceT m a)
-                         -> (Request -> (Response -> ResponseReceived) -> m a)
-                         -> Request
-                         -> (Response -> ResponseReceived)
-                         -> m a
-multipartHandlerOverride method path handler app req sendRes
+                         -> Middleware
+multipartHandlerOverride method path app req sendRes
     | requestMethod req == method
     , pathInfo req == path
     =  runResourceT $ do
           iState <- getInternalState
           let backend = tempFileBackEnd iState
           (params, files) <- liftIO $ parseRequestBody backend req
-          handler sendRes params files
+          let filename = case files of
+                          [] -> ""
+                          ((_, FileInfo{fileContent = fn}):_) -> fn
+              req' = req{requestBody = return . Text.encodeUtf8
+                                              $ Text.pack filename}
+
+          liftIO $ app req' sendRes
     | otherwise = app req sendRes
