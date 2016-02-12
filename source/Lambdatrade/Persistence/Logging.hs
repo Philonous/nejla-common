80ad56d8ed8374ef95f3c07827753f00158a4e85
Philipp Balzarek <p.balzarek@googlemail.com>
2016-02-12 13:27:58 +0100

    add request/response and critical event loggers

---
+++ source/Lambdatrade/Persistence/Logging.hs
@@ -0,0 +1,156 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DeriveDataTypeable #-}
+-- Copyright © 2014-2015 Lambdatrade AB. All rights reserved.
+
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+module Persistence.Transactions.Logging where
+
+import           Control.Applicative
+import qualified Control.Exception as Ex
+import           Control.Monad
+import           Data.ByteString (ByteString)
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Builder as BS
+import qualified Data.ByteString.Lazy as BSL
+import           Data.Data
+import           Data.IORef
+import qualified Data.List as List
+import           Data.Monoid
+import           Data.Text (Text)
+import qualified Data.Text as Text
+import qualified Data.Text.Encoding as Text
+import qualified Data.Text.Encoding.Error as Text
+import qualified Data.Text.IO as Text
+import           Data.Time.Clock (UTCTime)
+import           Data.Time.Clock (getCurrentTime)
+import qualified Data.Time.Clock as Time
+import           Data.Typeable
+import qualified Database.Persist.Sql as P
+import           GHC.Generics
+import qualified Network.HTTP.Types as HTTP
+import qualified Network.Wai as Wai
+import           System.IO (stderr)
+
+--------------------------------------------------------------------------------
+-- Request/Response log --------------------------------------------------------
+--------------------------------------------------------------------------------
+
+data RequestLog =
+  RequestLog
+    { requestLogTime    :: !UTCTime
+    , requestLogMethod  :: !Text
+    , requestLogPath    :: ![Text]
+    , requestLogQuery   :: !Text
+    , requestLogHeaders :: !Text
+    , requestLogBody    :: !Text
+    } deriving (Show, Typeable, Data, Generic)
+
+data ResponseLog id =
+  ResponseLog
+    { responseLogTime    :: !UTCTime
+    , responseLogRequest :: !id
+    , responseLogCode    :: !Int
+    , responseLogBody    :: !Text
+    } deriving (Show, Typeable, Data, Generic)
+
+logPublicCalls :: (RequestLog -> IO id)
+               -> (ResponseLog id -> IO a)
+               -> Wai.Middleware
+logPublicCalls logRequest logResponse app request' respond = do
+    now <- getCurrentTime
+    let bLength = readMaybe . Text.unpack . Text.decodeUtf8
+                    =<< List.lookup "content-length" (Wai.requestHeaders request')
+    (reqB, body) <- case bLength of
+        Just l | l < 2048 -> do
+                     body <- getBody (Wai.requestBody request') BS.empty
+                     bdRef <- newIORef body
+                     let rBody = do
+                             bd <- readIORef bdRef
+                             writeIORef bdRef BS.empty
+                             return  bd
+                     return (rBody, body)
+        _ -> return ( Wai.requestBody request'
+                    , "<body length unknown or greater 2048 bytes>")
+    let request = request'{Wai.requestBody = reqB}
+    reqId <- logRequest
+               RequestLog
+                 { requestLogTime =  now
+                 , requestLogMethod = bst $ Wai.requestMethod request
+                 , requestLogPath = Wai.pathInfo request
+                 , requestLogQuery =  bst $ Wai.rawQueryString request
+                 , requestLogHeaders =
+                     showText $ Wai.requestHeaders request
+                 , requestLogBody = bst body
+                 }
+    rr <- app request $ \response -> do
+        now' <- getCurrentTime
+        body <- responseToText response
+        logResponse
+            ResponseLog
+              { responseLogTime = now
+              , responseLogRequest = reqId
+              , responseLogCode =
+                  HTTP.statusCode $ Wai.responseStatus response
+              , responseLogBody = body
+              }
+        respond response
+    return rr
+  where
+    getBody nextChunk acc = do
+        chunk <- nextChunk
+        if BS.null chunk
+            then return acc
+            else getBody nextChunk (acc <> chunk)
+    showText = Text.pack . show
+    bst = Text.decodeUtf8
+    responseToText resp = do
+      ref <- newIORef []
+      case Wai.responseToStream resp of
+       (_, _, f) -> f $ \sb -> sb (\chunk -> modifyIORef ref (chunk:))
+                                  (return ())
+      chunks <- List.reverse <$> readIORef ref
+      return . Text.decodeUtf8With Text.lenientDecode . BSL.toStrict
+             . BS.toLazyByteString
+             $ mconcat chunks
+    readMaybe x = case reads x of
+                   ((r,_):_) -> Just r
+                   [] -> Nothing
+
+
+--------------------------------------------------------------------------------
+-- Critical Event --------------------------------------------------------------
+--------------------------------------------------------------------------------
+
+data CriticalEvent =
+  CriticalEvent
+    { criticalEventTime      :: !UTCTime
+    , criticalEventSystem    :: !Text
+    , criticalEventCondition :: !Text
+    , criticalEventContext   :: !Text
+    , criticalEventDetails   :: !Text
+    } deriving (Show, Typeable, Data, Generic)
+
+catchMiddleware :: (CriticalEvent -> IO ()) -> Wai.Middleware
+catchMiddleware logEvent app = \req cont ->
+    Ex.catch (app req cont)
+        (\e -> do
+              now <- getCurrentTime
+              Text.hPutStrLn stderr $ "[Error] Unhandled exception: "
+                                      <> showText (e :: Ex.SomeException)
+              Ex.catch ( logEvent $
+                  CriticalEvent
+                    { criticalEventTime = now
+                    , criticalEventSystem = "API"
+                    , criticalEventCondition = "unhandled exception"
+                    , criticalEventContext = ""
+                    , criticalEventDetails = showText e
+                    })
+                  (\e -> Text.hPutStrLn stderr $
+                         "[Error] Exception while trying to write to Critical Event log: "
+                         <> showText (e :: Ex.SomeException))
+              cont (Wai.responseBuilder HTTP.status500 [] ""))
+  where
+    showText = Text.pack . show
