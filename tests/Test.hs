ed6632907a4f62b955f939026f1ad2ec21a43764
Philipp Balzarek <p.balzarek@googlemail.com>
2016-11-07 14:35:02 +0100

    add tests for configuration (#12)

--- tests/Test.hs
+++ tests/Test.hs
@@ -5,10 +5,12 @@ import           Test.Tasty
 import qualified Persistent as Persistent
 import qualified Logging as Logging
 import qualified Logstash as Logstash
+import qualified Config as Config
 
 tests = testGroup "tests" [ Persistent.tests
                           , Logging.tests
                           , Logstash.tests
+                          , Config.tests
                           ]
 
 
