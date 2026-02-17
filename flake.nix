27d9dc189f9aeffff8d8a6901fcc7908547cbde9
Philipp Balzarek <p.balzarek@googlemail.com>
2026-02-17 20:20:22 +0100

    gitlab-ci.yml: Update to build with cabal

--- flake.nix
+++ flake.nix
@@ -7,27 +7,44 @@
 
   outputs = { self, nixpkgs }:
     let
-      system = "x86_64-linux";  # or aarch64-darwin, etc.
+      system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
       ghcVersion = "912";
       haskell = pkgs.haskell.packages."ghc${ghcVersion}";
-    in
-    {
-      devShells.${system}.default = pkgs.mkShell {
-        buildInputs = [
-          pkgs.docker-compose
-          pkgs.gnumake
-          pkgs.go-task
-          pkgs.postgresql_17
-          pkgs.postgresql_17.pg_config
-          pkgs.zlib
 
-          haskell.ghc
-          haskell.cabal-install
-          haskell.hpack
-          haskell.haskell-language-server
-          haskell.ormolu
-        ];
+      # Dependencies to build
+      buildDeps = [
+        pkgs.gnumake
+        pkgs.go-task
+        pkgs.postgresql_17
+        pkgs.zlib
+        pkgs.pkg-config
+        haskell.ghc
+        haskell.cabal-install
+        haskell.hpack
+      ];
+      # Dependencies for local development
+      devDeps  = [
+        pkgs.docker-compose
+        haskell.haskell-language-server
+        haskell.ormolu
+      ];
+    in
+      {
+        devShells.${system} = {
+          # Development shell
+          default = pkgs.mkShell {
+            buildInputs = buildDeps ++ devDeps;
+            # Local dev probably has this set up anyway, but it doesn't hurt
+            LOCALE_ARCHIVE = "${pkgs.glibcLocales}/lib/locale/locale-archive";
+            LANG = "en_US.UTF-8";
+          };
+          # Shell for building in CI
+          ci = pkgs.mkShell {
+            buildInputs = buildDeps;
+            LOCALE_ARCHIVE = "${pkgs.glibcLocales}/lib/locale/locale-archive";
+            LANG = "en_US.UTF-8";
+          };
+        };
       };
-    };
 }
